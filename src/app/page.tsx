4137de6943a7cfedd93b27ffbf945287fc7937ae
Kristjan Rajaniemi <kristjan.rajaniemi@live.fi>
2025-01-10 12:17:28 +0200

    link to hcbull soundcloud and hcbull logo to link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
 	return (
@@ -37,27 +38,27 @@ export default function Home() {
 				<div className="flex gap-4 items-center flex-col sm:flex-row">
 					<a
 						className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-						href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-						target="_blank"
+						href="soundcloud.com/hardcore-bull"
+						target="_self"
 						rel="noopener noreferrer"
 					>
 						<Image
-							className="dark:invert"
-							src="/vercel.svg"
-							alt="Vercel logomark"
+							className=""
+							src="/HCMUSCLEBULL.png"
+							alt="HARDCORE BULL LOGO"
 							width={20}
 							height={20}
 						/>
-						Deploy now
+						Listen to HARDCORE BULL
 					</a>
-					<a
+					<Link
 						className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-						href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+						href=""
 						target="_blank"
 						rel="noopener noreferrer"
 					>
-						Read our docs
-					</a>
+						HARDCORE BULL Discord Bot statistics
+					</Link>
 				</div>
 			</main>
 			<footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
